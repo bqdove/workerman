08cf7cfc74bb4640b8771c34d657b110949bb3eb
twilroad <269044570@qq.com>
2017-05-28 15:29:32 +0800

    fix author email and organization

--- src/Injections/Installer.php
+++ src/Injections/Installer.php
@@ -2,8 +2,8 @@
 /**
  * This file is part of Notadd.
  *
- * @author TwilRoad <269044570@qq.com>
- * @copyright (c) 2016, notadd.com
+ * @author TwilRoad <heshudong@ibenchu.com>
+ * @copyright (c) 2017, notadd.com
  * @datetime 2017-05-22 11:32
  */
 namespace Notadd\Workerman\Injections;
